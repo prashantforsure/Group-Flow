6a9c3091a52203551bd3bef92d568b2479af514f
prashantforsure <prashantforsure@gmail.com>
2024-10-08 08:10:17 +0530

    task/[id] api

--- src/app/api/tasks/[id]/route.ts
+++ src/app/api/tasks/[id]/route.ts
@@ -1,9 +1,10 @@
 import { authOptions } from "@/lib/auth/config";
 import prisma from "@/lib/db";
+import { TaskUpdateSchema } from "@/lib/validations/taskUpdate";
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req: NextRequest, { params }: { params : {id: string } } ){
+export async function GET(req: NextRequest, { params }: { params : { id: string } } ){
 try{
     const session = await getServerSession(authOptions)
     if(!session?.user?.email){
@@ -77,5 +78,74 @@ try{
      return NextResponse.json(task)
 }catch(error){
     return NextResponse.json({ error: 'Internal server error' }, { status: 500 });
+}
+}
+
+export async function PUT(req: NextRequest, { params }: { params : { id: string } }){
+try{
+const session = await getServerSession(authOptions)
+if(!session?.user?.email){
+  return NextResponse.json({
+    message: " unauth"
+  }, {
+    status: 401
+  })
+}
+const body = await req.json();
+const validatedData = TaskUpdateSchema.parse(body)
+
+const task = await prisma.task.findUnique({
+  where: {
+    id: params.id
+  }, include: {
+    group: true
+  }
+})
+if(!task){
+  return NextResponse.json({
+    message: "no task found"
+  }, {
+    status: 404
+  })
+}
+   const isMember = await prisma.groupMember.findFirst({
+    where: {
+      groupId: task.groupId
+    }
+   })
+   if (!isMember) {
+    return NextResponse.json({ error: 'Access denied' }, { status: 403 });
+  }
+  const updatedTask = await prisma.task.update({
+    where: { id: params.id },
+    data: validatedData,
+    include: {
+      creator: {
+        select: {
+          id: true,
+          name: true,
+          email: true,
+          avatar: true,
+        }
+      },
+      assignments: {
+        include: {
+          assignee: {
+            select: {
+              id: true,
+              name: true,
+              email: true,
+              avatar: true,
+            }
+          }
+        }
+      },
+      group: true,
+    }
+  });
+  return NextResponse.json(task)
+}catch(error){
+  return NextResponse.json({ error: 'Internal server error' },
+     { status: 500 });
 }
 }
